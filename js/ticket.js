7edbc9dce4089485fc38f003a603dd4cf9a8300e
devgreact <dev.greact@gmail.com>
2025-06-13 18:05:04 +0900

    [docs] 반응형 테스트 진행중

--- js/ticket.js
+++ js/ticket.js
@@ -1,7 +1,7 @@
 window.addEventListener("load", function () {
   // swiper 만들기 실행
   new Swiper(".sw_ticket", {
-    slidesPerView: 8,
+    slidesPerView: 3,
     spaceBetween: 10,
     slidesPerGroup: 1,
     navigation: {
@@ -9,10 +9,6 @@ window.addEventListener("load", function () {
       prevEl: ".ticket_slide_prev",
     },
     breakpoints: {
-      960: {
-        slidesPerView: 2,
-        spaceBetween: 20,
-      },
       1024: {
         slidesPerView: 3,
         spaceBetween: 20,
